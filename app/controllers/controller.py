611b28e5f36d0feb6841c7ed5c21ea4d1b681861
Piotr <pigryko@yahoo.com>
2021-03-12 23:54:49 +0000

    Extensions 5

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,8 +1,11 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, g
 from app import app
 from app.models.game import Game
 from app.models.player import Player
 import random
+
+global player_1_name 
+global player_2_name
 
 @app.route('/')
 def index():
@@ -25,7 +28,8 @@ def p2choice(choice1):
 
 @app.route('/cpu/gamestart_cpu')
 def gamestart_cpu():
-    return render_template('gamestart_cpu.html')
+    # player_1_name = create_name_cpu.player_1_name
+    return render_template('gamestart_cpu.html', p1name = player_1_name)
 
 @app.route('/cpu/<choice1_cpu>')
 def p2choice_cpu(choice1_cpu):
@@ -35,7 +39,18 @@ def p2choice_cpu(choice1_cpu):
 
 @app.route('/cpu/<choice1>/<choice2>')
 def result_cpu(choice1, choice2):
-    player_1 = Player("Player 1", choice1)
+    global player_1_name
+    player_1 = Player(player_1_name, choice1)
     player_2 = Player("Computer", choice2)
     new_result = Game.game_result(player_1, player_2)
     return render_template('result_cpu.html', result=new_result)
+
+@app.route('/get_name_cpu')
+def get_name_cpu():
+    return render_template('get_name_cpu.html')
+
+@app.route('/get_name_cpu', methods=['POST'])
+def create_name_cpu():
+    global player_1_name
+    player_1_name = request.form['name1']
+    return render_template('get_name_cpu.html')
