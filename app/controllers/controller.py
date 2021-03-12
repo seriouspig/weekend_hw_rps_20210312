b9c8c7cb2bdc796942495a72764cb3962dce0e8a
Piotr <pigryko@yahoo.com>
2021-03-12 22:49:52 +0000

    Extensions 4

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect
 from app import app
 from app.models.game import Game
 from app.models.player import Player
+import random
 
 @app.route('/')
 def index():
@@ -22,3 +23,19 @@ def gamestart():
 def p2choice(choice1):
     return render_template('p2choice.html')
 
+@app.route('/cpu/gamestart_cpu')
+def gamestart_cpu():
+    return render_template('gamestart_cpu.html')
+
+@app.route('/cpu/<choice1_cpu>')
+def p2choice_cpu(choice1_cpu):
+    weapon_list = ["rock", "paper", "scissors"]
+    cpu_choice = random.choice(weapon_list)
+    return render_template('p2choice_cpu.html', cpu_choice = cpu_choice)
+
+@app.route('/cpu/<choice1>/<choice2>')
+def result_cpu(choice1, choice2):
+    player_1 = Player("Player 1", choice1)
+    player_2 = Player("Computer", choice2)
+    new_result = Game.game_result(player_1, player_2)
+    return render_template('result_cpu.html', result=new_result)
--- app/models/player.py
+++ app/models/player.py
@@ -1,5 +1,12 @@
+import random
+
 class Player():
 
     def __init__(self, name, choice):
         self.name = name
         self.choice = choice
+
+
+    def random_cpu():
+        weapon_list = ["rock", "paper", "scissors"]
+        return random.choice(weapon_list)
